a237d4c1f1b3b49edf5e769004773701a58fdb8e
hdffede <fede@hdfgroup.org>
2016-05-03 22:02:31 -0500

    adding header with copyright

--- src/autotuner_hdf5_static.c
+++ src/autotuner_hdf5_static.c
@@ -1,3 +1,14 @@
+/* 
+* Copyright by The HDF Group.
+* All rights reserved.
+*
+* This file is part of h5tuner. The full h5tuner copyright notice,
+*  including terms governing use, modification, and redistribution, is
+* contained in the file COPYING, which can be found at the root of the
+* source code distribution tree.  If you do not have access to this file,
+* you may request a copy from help@hdfgroup.org.
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
